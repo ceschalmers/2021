18258ca2b1edec1b45942848b7688ee7ed863106
emslGit <emanuel@keemail.me>
2020-10-19 21:52:38 +0200

    Deactivated parallax on touch
    Merge pull request #7 from ceschalmers/responsive

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.sass']
 })
-export class LandingComponent implements OnInit {
+export class LandingComponent {
+  ratio1: number
+  ratio2: number
+  ratio3: number
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    if (!navigator.maxTouchPoints) {
+      this.ratio1 = 0.75
+      this.ratio2 = 0.45
+      this.ratio3 = 0.25
+    } else {
+      this.ratio1 = 1
+      this.ratio2 = 1
+      this.ratio3 = 1
+    }
+    console.log(!!navigator.maxTouchPoints)
   }
-
 }
